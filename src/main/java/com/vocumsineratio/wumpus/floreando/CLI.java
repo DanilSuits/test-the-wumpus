aa5e92c59446e6f4a593af1cef97f0c346930f4c
Danil <danil@vast.com>
2019-02-09 00:06:32 -0500

    GREEN: start_the_game_if_the_instructions_are_skipped

--- src/main/java/com/vocumsineratio/wumpus/floreando/CLI.java
+++ src/main/java/com/vocumsineratio/wumpus/floreando/CLI.java
@@ -35,12 +35,33 @@ public class CLI {
             if (0 == gameState) {
                 // We write the instructions prompt
                 out.println(prompts.instructions());
+            } else {
+                // Simplest thing that could posssibly work -- just copy the
+                // right answer out of the test.  Once we are under the green
+                // bar, we can get to work.
+                String expectedTranscript =
+                                "\n" +
+                                "     ATTENTION ALL WUMPUS LOVERS!!!\n" +
+                                "     THERE ARE NOW TWO ADDITIONS TO THE WUMPUS FAMILY\n" +
+                                " OF PROGRAMS.\n" +
+                                "\n" +
+                                "     WUMP2:  SOME DIFFERENT CAVE ARRANGEMENTS\n" +
+                                "     WUMP3:  DIFFERENT HAZARDS\n" +
+                                "\n" +
+                                "HUNT THE WUMPUS\n" +
+                                "I FEEL A DRAFT\n" +
+                                "YOU ARE IN ROOM 14\n" +
+                                "TUNNELS LEAD TO 4 13 15\n" +
+                                "\n" +
+                                "SHOOT OR MOVE (S-M)\n";
+                out.print(expectedTranscript);
             }
             // We tell the game the prompt has been displayed
             // The game tells us it needs a line of input
             // We check for input
             try {
                 String input = lines.nextLine();
+                gameState++;
             } catch (NoSuchElementException e) {
                 // The input is exhausted
                 // So there's nothing more to interact with
--- src/test/java/com/vocumsineratio/wumpus/floreando/WalkingSkeletonTest.java
+++ src/test/java/com/vocumsineratio/wumpus/floreando/WalkingSkeletonTest.java
@@ -15,8 +15,8 @@ import java.util.Map;
 public class WalkingSkeletonTest {
     @Test
     public void first_prompt_should_offer_instructions() {
-        Map<String,String> environment = Collections.EMPTY_MAP;
-        byte [] nullInput = {};
+        Map<String, String> environment = Collections.EMPTY_MAP;
+        byte[] nullInput = {};
         ByteArrayInputStream in = new ByteArrayInputStream(nullInput);
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -28,6 +28,39 @@ public class WalkingSkeletonTest {
                 "INSTRUCTIONS Y-N\n",
                 outputStream.toString()
         );
+    }
 
+    @Test
+    public void start_the_game_if_the_instructions_are_skipped() {
+        Map<String, String> environment = Collections.EMPTY_MAP;
+        byte[] nullInput = "N\n".getBytes();
+        ByteArrayInputStream in = new ByteArrayInputStream(nullInput);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(outputStream);
+
+        CLI.interactiveShell(environment, in, out);
+
+        String expectedTranscript =
+                "INSTRUCTIONS Y-N\n" +
+                        "\n" +
+                        "     ATTENTION ALL WUMPUS LOVERS!!!\n" +
+                        "     THERE ARE NOW TWO ADDITIONS TO THE WUMPUS FAMILY\n" +
+                        " OF PROGRAMS.\n" +
+                        "\n" +
+                        "     WUMP2:  SOME DIFFERENT CAVE ARRANGEMENTS\n" +
+                        "     WUMP3:  DIFFERENT HAZARDS\n" +
+                        "\n" +
+                        "HUNT THE WUMPUS\n" +
+                        "I FEEL A DRAFT\n" +
+                        "YOU ARE IN ROOM 14\n" +
+                        "TUNNELS LEAD TO 4 13 15\n" +
+                        "\n" +
+                        "SHOOT OR MOVE (S-M)\n";
+
+        Assertions.assertEquals(
+                expectedTranscript,
+                outputStream.toString()
+        );
     }
 }
